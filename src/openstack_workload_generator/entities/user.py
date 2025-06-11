6b867996aa45dabc07f322aae13809b70d5954e7
Marc Schöchlin <marc.schoechlin@uhurutec.com>
2025-06-11 16:47:57 +0200

    reformat
    Signed-off-by: Marc Schöchlin <marc.schoechlin@uhurutec.com>

--- src/openstack_workload_generator/entities/user.py
+++ src/openstack_workload_generator/entities/user.py
@@ -51,7 +51,7 @@ class WorkloadGeneratorUser:
         )
         self.assign_role_to_user("manager")
         LOGGER.info(
-            f"Created user {self.obj.name} / {self.obj.id} with password >>>{self.obj.password}<<< " 
+            f"Created user {self.obj.name} / {self.obj.id} with password >>>{self.obj.password}<<< "
             f"in {DomainCache.ident_by_id(self.domain.id)}"
         )
         return self.obj
